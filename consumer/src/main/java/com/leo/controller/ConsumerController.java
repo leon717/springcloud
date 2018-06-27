39898c7cf0127917686b4537f011d531743508c0
leonhardt <leonhardt@67472a50-569b-5b47-acf6-b9010f8fb196>
2018-06-27 15:28:49 +0000

    git-svn-id: https://leonhardt-pc/svn/springcloud/%E6%BA%90%E7%A8%8B%E5%BA%8F@3 67472a50-569b-5b47-acf6-b9010f8fb196

---
+++ consumer/src/main/java/com/leo/controller/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.leo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leo.client.ProducerClient;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    ProducerClient producer;
+    
+    @GetMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return producer.hello(name);
+    }
+}
