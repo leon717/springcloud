1598e02aa0204921a8a642315bb7ecead30a8a0b
leonhardt <leonhardt@67472a50-569b-5b47-acf6-b9010f8fb196>
2018-06-29 02:22:04 +0000

    添加熔断器Hystrix
    git-svn-id: https://leonhardt-pc/svn/springcloud/%E6%BA%90%E7%A8%8B%E5%BA%8F@6 67472a50-569b-5b47-acf6-b9010f8fb196

--- consumer/src/main/java/com/leo/client/ProducerClient.java
+++ consumer/src/main/java/com/leo/client/ProducerClient.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name= "producer")
+import com.leo.client.hystrix.ProducerHystrix;
+
+@FeignClient(name= "producer", fallback = ProducerHystrix.class)
 public interface ProducerClient {
 	
     @GetMapping(value = "/hello")
