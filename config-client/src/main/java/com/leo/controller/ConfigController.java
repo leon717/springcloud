383a6b3027f3c07a7e303fb235a5ebbef7676c81
leo <717626659@qq.com>
2018-07-05 11:04:57 +0800

    运行期更新config

--- config-client/src/main/java/com/leo/controller/ConfigController.java
+++ config-client/src/main/java/com/leo/controller/ConfigController.java
@@ -1,9 +1,11 @@
 package com.leo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope		//使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中，通过/refresh请求（POST）刷新	
 @RestController
 public class ConfigController {
 
