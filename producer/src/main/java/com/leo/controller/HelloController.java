bdebb7c46954f4c7897656f50b49059f42f48c3f
leo <717626659@qq.com>
2018-07-14 16:54:17 +0800

    zuul网关filter，熔断，重试

--- producer/src/main/java/com/leo/controller/HelloController.java
+++ producer/src/main/java/com/leo/controller/HelloController.java
@@ -1,13 +1,29 @@
 package com.leo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
 	@GetMapping("hello")
 	public String hello(String name){
 		return "hello "+name+"!";
+	}
+	
+	//only for zuul retry test
+	@GetMapping("zuul")
+	public String index(String name) {
+	    logger.info("request two name is "+name);
+	    try{
+	        Thread.sleep(1000000);
+	    }catch ( Exception e){
+	        logger.error(" hello two error ",e);
+	    }
+	    return "hello "+name+"，this is two messge";
 	}
 }
