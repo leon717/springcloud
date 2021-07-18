a1008f708f91fdeb7b093a3d12d06d2f9a6d14fe
Leo <717626659@qq.com>
2021-07-18 19:48:17 +0800

    eureka

--- eureka-client/src/main/java/com/leo/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/leo/EurekaClientApplication.java
@@ -3,12 +3,14 @@ package com.leo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
 }
--- eureka-server/src/main/java/com/leo/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/leo/EurekaServerApplication.java
@@ -3,12 +3,14 @@ package com.leo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
