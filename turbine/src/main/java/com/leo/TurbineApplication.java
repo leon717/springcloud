6e7392b2aea49c07f8aa3913088e089c4733f99c
Leo <717626659@qq.com>
2021-07-19 22:44:47 +0800

    turbine

--- turbine/src/main/java/com/leo/TurbineApplication.java
+++ turbine/src/main/java/com/leo/TurbineApplication.java
@@ -2,15 +2,17 @@ package com.leo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableHystrixDashboard
+@EnableHystrixDashboard        // http://localhost:8083/hystrix
 @EnableTurbine
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TurbineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TurbineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
 }
